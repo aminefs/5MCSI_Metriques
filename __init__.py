227685eb2a9dbabfcef8d16ce8891f766c2b0d99
mennane mohamed amine <83577134+aminefs@users.noreply.github.com>
2024-09-12 16:52:23 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,36 +37,25 @@ def monhistogramme():
 
 
 
-# Route pour extraire les minutes à partir d'une date
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return jsonify({'minutes': minutes})
-
-# Route pour récupérer les commits du repo et compter les commits par minute
 @app.route('/commits/')
-def get_commits():
-    url = 'https://github.com/aminefs/5MCSI_Metriques/blob/main/templates/commits.html'
+def get_commits_by_hour():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
     response = requests.get(url)
     data = response.json()
 
-    commits_by_minute = {}
+    commits_by_hour = {}
 
-    # Parcourir les commits et extraire les minutes
+    # Parcourir les commits et extraire les heures
     for commit in data:
         commit_date = commit['commit']['author']['date']
-        minutes = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ').minute
+        hour = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ').hour
         
-        if minutes in commits_by_minute:
-            commits_by_minute[minutes] += 1
+        if hour in commits_by_hour:
+            commits_by_hour[hour] += 1
         else:
-            commits_by_minute[minutes] = 1
+            commits_by_hour[hour] = 1
 
-    return jsonify(commits_by_minute)
-
-
-
+    return jsonify(commits_by_hour)
 
   
 if __name__ == "__main__":
